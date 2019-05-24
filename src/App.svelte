d7f9fea9b253dba7bf5e30370834a260b20586fd
stefian <v_ianta@yahoo.com>
2019-05-24 12:08:06 -0400

    changed to sending individual skills

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,44 @@
 <script>
-    let hobbies = [];
-    let hobbyInput;
-    
-    function addHobby() {
-        hobbies = [...hobbies, hobbyInput.value];
-    
-        fetch('https://svelte-http.firebaseio.com/skills.json', {
-            method: 'POST',
-            body: JSON.stringify(hobbies),
-            headers: {
-                'content-Type': 'application/json'
-            }
-        }).then(res => {
-            if (!res.ok) {
-                throw new Error('Failed!');
-            }
-            // ... 
-        }).catch(err => {
-            console.log(err);
-        });
-    }
+  let hobbies = [];
+  let hobbyInput;
+  let isLoading = false;
+
+  function addHobby() {
+    hobbies = [...hobbies, hobbyInput.value];
+
+    isLoading = true;
+    fetch("https://svelte-http.firebaseio.com/skills.json", {
+      method: "POST",
+      body: JSON.stringify(hobbyInput.value),
+      headers: {
+        "content-Type": "application/json"
+      }
+    })
+      .then(res => {
+        isLoading = false;
+        if (!res.ok) {
+          throw new Error("Failed!");
+        }
+        // ...
+        alert("Saved Data");
+      })
+      .catch(err => {
+        isLoading = false;
+        console.log(err);
+      });
+  }
 </script>
 
 <label for="hobby">Skills</label>
-<input type="text" id="hobby" bind:this={hobbyInput}>
+<input type="text" id="hobby" bind:this={hobbyInput} />
 <button on:click={addHobby}>Add Skills</button>
 
-<ul>
-{#each hobbies as hobby}
-    <li>{hobby}</li>
-{/each}
-</ul>
+{#if isLoading}
+  <p>Loading...</p>
+{:else}
+  <ul>
+    {#each hobbies as hobby}
+      <li>{hobby}</li>
+    {/each}
+  </ul>
+{/if}
