2ea9c00caa9b7fcb899e183d40e84ae3fcd76aa0
stefian <v_ianta@yahoo.com>
2019-05-24 11:44:03 -0400

    changed labels to Skills

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,18 @@
 <script>
-	
+    let hobbies = [];
+    let hobbyInput;
+    
+    function addHobby() {
+        hobbies = [...hobbies, hobbyInput.value]
+    }
 </script>
 
+<label for="hobby">Skills</label>
+<input type="text" id="hobby" bind:this={hobbyInput}>
+<button on:click={addHobby}>Add Skills</button>
 
+<ul>
+{#each hobbies as hobby}
+    <li>{hobby}</li>
+{/each}
+</ul>
